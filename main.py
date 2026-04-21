3f8d8a872383808582014df9ae34a25024d9c0a3
Edmond Volosciuc <volosciucedmond@gmail.com>
2026-04-21 15:53:58 +0300

    feat: JSON based vehicle databsed and dynamic specs lookup

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from src import analysis, visualization
 filePath = 'data/drive_cycle.csv'
 print("Starting analysis of trip data...")
 
-trip_data = analysis.analyze_trip_data(filePath)
+data_tesla = analysis.analyze_trip_data(filePath, vehicle_name="tesla_model_3")
+data_dacia = analysis.analyze_trip_data(filePath, vehicle_name="dacia_spring")
+data_audi = analysis.analyze_trip_data(filePath, vehicle_name="audi_etron_gt")
+data_vw = analysis.analyze_trip_data(filePath, vehicle_name="vw_id3")
+
 print("Trip data analysis complete. Generating visualizations...")
 
-visualization.plot_results(trip_data)
+visualization.plot_results(data_tesla, "tesla_model_3")
+visualization.plot_results(data_dacia, "dacia_spring")
+visualization.plot_results(data_audi, "audi_etron_gt")
+visualization.plot_results(data_vw, "vw_id3")
--- src/analysis.py
+++ src/analysis.py
@@ -1,27 +1,33 @@
 from src.physics import convert_kmh_to_ms, calculate_drag, calculate_rolling_resistance, calculate_acceleration_force, calculate_total_power
-
 import pandas as pd
+import json
 
+with open('data/car_data.json', 'r') as f:
+    car_data = json.load(f)
 
-
-def analyze_trip_data(file_path):
+def analyze_trip_data(file_path, vehicle_name):
     df = pd.read_csv(file_path)
+    mass = car_data[vehicle_name]["mass_kg"]
+    drag_coefficient = car_data[vehicle_name]["drag_coefficient"]
+    frontal_area = car_data[vehicle_name]["frontal_area_m2"]
+    crr = car_data[vehicle_name]["crr"]
+    
 
     df["velocity"] = convert_kmh_to_ms(df["speed(km/h)"])
-    df["drag_force"] = calculate_drag(df["velocity"], drag_coefficient=0.3, area=2.2)
-    df["rolling_resistance"] = calculate_rolling_resistance(mass=1500, rolling_resistance_coefficient=0.015)
-    df["acceleration_force"] = calculate_acceleration_force(mass=1500, acceleration=df["acceleration(m/s²)"])
+    df["drag_force"] = calculate_drag(df["velocity"], drag_coefficient=drag_coefficient, area=frontal_area)
+    df["rolling_resistance"] = calculate_rolling_resistance(mass=mass, rolling_resistance_coefficient=crr)
+    df["acceleration_force"] = calculate_acceleration_force(mass=mass, acceleration=df["acceleration(m/s²)"])
     df["total_force"] = df["drag_force"] + df["rolling_resistance"] + df["acceleration_force"]
     df["power"] = calculate_total_power(df["velocity"], df["total_force"])
     df["energy_j"] = df["power"] * 1
     df["cumulative_energy_kwh"] = df["energy_j"].cumsum() / 3.6e6
 
-    total_joules = sum(df["power"])
-    total_distance = sum(df["velocity"])
+    total_joules = df["power"].sum()
+    total_distance = df["velocity"].sum()
     # print(total_distance)
     # print(total_joules)
 
     kwh_per_100km = (total_joules / 3.6e6) / (total_distance / 100000)
-    print(f"Energy consumption: {kwh_per_100km:.2f} kWh/100km")
+    print(f"Energy consumption for {car_data[vehicle_name]['name']}: {kwh_per_100km:.2f} kWh/100km")
     # print(trip_data)
     return df
